63346d4456521ec0f1b43c5c8369fa0e0484f1e8
StevenWhitaker <steventwhitaker@gmail.com>
2020-05-15 06:17:33 -0400

    Add tests for `expm` and the derivatives of `frexp1` and `frexp2`.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using BlochSim
+using ForwardDiff
 using MAT
 using Test
 @testset "BlochSim.jl" begin
     include("helpers.jl")
+    include("expm.jl")
     include("Spin.jl")
     include("sequences.jl")
 end
