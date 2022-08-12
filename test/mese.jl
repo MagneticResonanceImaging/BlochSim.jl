d5f13739756245df04a4281f12b65860d5976c34
StevenWhitaker <steventwhitaker@gmail.com>
2022-08-12 14:01:45 -0400

    Update `MESEBlochSim` to include rephasing and crusher gradients and spoiling.

--- test/mese.jl
+++ test/mese.jl
@@ -2,14 +2,18 @@ function mese1()
 
     rfex = RF([π/2], [0], 0, 0, Gradient(0, 0, 0))
     rfref = RF([π], [-π/2], 0, 0, Gradient(0, 0, 0))
-    mese! = MESEBlochSim(3000, 10, 32, rfex, rfref)
+    rephaser = GradientSpoiling(0, 0, 0, 0)
+    crusher = GradientSpoiling(0, 0, 0, 0)
+    spoiler = GradientSpoiling(0, 0, 0, 0)
+    mese1! = MESEBlochSim(3000, 10, 32, rfex, rfref, rephaser, crusher, spoiler)
+    mese2! = MESEBlochSim(3000, 10, 32, rfex, rfref, rephaser, crusher, nothing)
     s1 = Spin(1, 1000, 100, 0)
     s2 = SpinMC(1, (1, 0), (1000, 100), (100, 10), (0, 0), (Inf, Inf))
-    M1 = mese!(s1)
-    M2 = mese!(s2)
+    M1 = mese1!(s1)
+    M2 = mese2!(s2)
 
-    show(devnull, mese!)
-    show(devnull, "text/plain", mese!)
+    show(devnull, mese1!)
+    show(devnull, "text/plain", mese1!)
     return all(M1[i] ≈ M2[i][1] for i = 1:32)
 
 end
