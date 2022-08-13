2f9b91a5cf3cbc5fcb51833a1c2f1d638ad77be8
StevenWhitaker <steventwhitaker@gmail.com>
2022-08-12 22:50:10 -0400

    Add tests for coverage.

--- test/mese.jl
+++ test/mese.jl
@@ -2,7 +2,7 @@ function mese1()
 
     rfex = RF([π/2], [0], 0, 0, Gradient(0, 0, 0))
     rfref = RF([π], [-π/2], 0, 0, Gradient(0, 0, 0))
-    rephaser = GradientSpoiling(0, 0, 0, 0)
+    rephaser = GradientSpoiling([0], [0], [0], 0)
     crusher = GradientSpoiling(0, 0, 0, 0)
     spoiler = GradientSpoiling(0, 0, 0, 0)
     mese1! = MESEBlochSim(3000, 10, 32, rfex, rfref, rephaser, crusher, spoiler)
@@ -10,11 +10,12 @@ function mese1()
     s1 = Spin(1, 1000, 100, 0)
     s2 = SpinMC(1, (1, 0), (1000, 100), (100, 10), (0, 0), (Inf, Inf))
     M1 = mese1!(s1)
-    M2 = mese2!(s2)
+    M2 = mese1!(s2)
+    M3 = mese2!(s2)
 
     show(devnull, mese1!)
     show(devnull, "text/plain", mese1!)
-    return all(M1[i] ≈ M2[i][1] for i = 1:32)
+    return all(M1[i] ≈ M2[i][1] ≈ M3[i][1] for i = 1:32)
 
 end
 
