901a86635c6b8aee4be485bae5433b29fc6141e7
Jeff Fessler <fessler@umich.edu>
2026-04-19 13:05:04 -0400

    test muting expm

--- src/expm-bloch3.jl
+++ src/expm-bloch3.jl
@@ -1,16 +1,73 @@
 #=
 expm-bloch3.jl
 Compute matrix exponential exp(A * t)
-where A is the 3×3 Bloch matrix.
+where A is the 3×3 Bloch matrix (for a constant RF and gradient amplitude),
+using an explicit eigendecomposition
+based on analytical roots of cubic characteristic polynomial.
 
 This code was developed with the help of GPT 5.2.
 =#
 
-using LinearAlgebra: cond, cross, norm, Diagonal
+using LinearAlgebra: cond, norm, Diagonal, lu!, mul!, rdiv!
 
-export expm_bloch3
+export expm_bloch3, expm_bloch3!
 
 const Breal = Number # Real or ForwardDiff.Dual
+
+
+#=
+import LinearAlgebra: cross
+
+"""
+    cross(a::NTuple{3}, b::NTuple{3})
+Return cross-product for 3-tuples;
+avoids allocations.
+"""
+function cross(a::NTuple{3,Breal}, b::NTuple{3,Breal})
+    a1, a2, a3 = a
+    b1, b2, b3 = b
+    return (a2*b3-a3*b2, a3*b1-a1*b3, a1*b2-a2*b1)
+end
+=#
+
+
+"""
+    Bloch3ExpmWorkspace{T<:Real}
+
+Workspace for computing the matrix exponential
+of a 3×3 Bloch matrix.
+"""
+struct Bloch3ExpmWorkspace{T <: Breal}
+    row1::Vector{Complex{T}}
+    row2::Vector{Complex{T}}
+    row3::Vector{Complex{T}}
+    λ::Vector{Complex{T}}
+    V::Matrix{Complex{T}} # eigenvectors of A
+    VD::Matrix{Complex{T}} # V * Diagonal(exp.(λ * t))
+end
+
+# constructor
+Bloch3ExpmWorkspace{T}() where {T <: Breal} = Bloch3ExpmWorkspace(
+    ntuple(_ -> Vector{Complex{T}}(undef, 3), 4)...,
+    ntuple(_ -> Matrix{Complex{T}}(undef, 3, 3), 2)...,
+)
+
+
+"""
+    cross!(v::Vector, a::AbstractVector, b::AbstractVector)
+Compute cross-product `v = a ⊗ b` for 3-vectors;
+avoids allocations by mutating input `v`.
+"""
+function cross!(
+    v::AbstractVector{<:Number},
+    a::AbstractVector{<:Number},
+    b::AbstractVector{<:Number},
+)
+    v[1] = a[2] * b[3] - a[3] * b[2]
+    v[2] = a[3] * b[1] - a[1] * b[3]
+    v[3] = a[1] * b[2] - a[2] * b[1]
+    return v
+end
 
 
 """
@@ -94,27 +151,63 @@ function eigvals_bloch3(
 end
 
 
+function fill3!(v::Vector, a, b, c)
+    v[1] = a
+    v[2] = b
+    v[3] = c
+end
+
+
 """
-    eigvec_bloch3(r1, r2, w, s, c, λ)
-Compute eigenvector for eigenvalue λ by crossing two rows;
+    eigvec_bloch3!(v, row1, row2, row3, r1, r2, w, s, c, λ)
+Compute eigenvector `v` for eigenvalue λ by crossing two rows;
 switch row pairs if needed.
+Mutates `v` and workspace `row1` `row2` `row3`
 """
+function eigvec_bloch3!(
+    v::AbstractVector{Complex{T}},
+    row1::Vector{Complex{T}},
+    row2::Vector{Complex{T}},
+    row3::Vector{Complex{T}},
+    r1::Breal, r2::Breal, w::Breal, s::Breal, c::Breal, λ::Complex{<:Breal};
+    rtol::Real = 1e-12,
+) where {T <: Breal}
+
+    fill3!(row1, -r2-λ, w,     s)
+    fill3!(row2, -w,   -r2-λ,  c)
+    fill3!(row3, -s,   -c,   -r1-λ)
+    rmax = max(norm(row1), norm(row2), norm(row3))
+
+    cross!(v, row1, row2)
+    if norm(v) < rtol * rmax
+        cross!(v, row1, row3)
+    end
+    if norm(v) < rtol * rmax
+        cross!(v, row2, row3)
+    end
+    return v
+end
+
+
+#=
+# even though this version used only small tuples, it still allocated
+
 function eigvec_bloch3(
     r1::T, r2::T, w::T, s::T, c::T, λ::Complex{T};
     rtol::Real = 1e-12,
 ) where {T <: Breal}
 
-    R1 = [-r2-λ, w,     s]
-    R2 = [-w,   -r2-λ,  c]
-    R3 = [-s,   -c,   -r1-λ]
-    rmax = maximum(norm, (R1, R2, R3))
+    row1 = (-r2-λ, w,     s)
+    row2 = (-w,   -r2-λ,  c)
+    row3 = (-s,   -c,   -r1-λ)
+    rmax = max(norm(row1), norm(row2), norm(row3))
 
-    v = cross(R1, R2)
+    v = cross(row1, row2)
     if norm(v) < rtol * rmax
-        v = cross(R1, R3)
+        v = cross(row1, row3)
     end
     if norm(v) < rtol * rmax
-        v = cross(R2, R3)
+        v = cross(row2, row3)
     end
     return v
 end
@@ -127,36 +220,90 @@ function eigvec_bloch3(
     T = promote_type(T1, T2, Tw, Ts, Tc, Tλ, Float32)
     return eigvec_bloch3(T(r1), T(r2), T(w), T(s), T(c), Complex{T}(λ))
 end
+=#
 
 
 """
-    (λ, V) = eigen_bloch3(r1, r2, w, s, c)
+    (λ, V) = eigen_bloch3!(λ, V, row1, row2, row3, r1, r2, w, s, c)
 Return eigendecomposition `(λ, V)`
 of 3×3 Bloch matrix
 `A = [-r2  w   s;
       -w  -r2  c;
       -s  -c  -r1]`
+Mutates `λ`, `V` and workspace `row1 row2 row3`, so allocation free.
 """
-function eigen_bloch3(r1::T, r2::T, w::T, s::T, c::T) where {T <: Breal}
+function eigen_bloch3!(
+    λ::Vector{Complex{Tw}},
+    V::Matrix{Complex{Tw}},
+    row1::Vector{Complex{Tw}},
+    row2::Vector{Complex{Tw}},
+    row3::Vector{Complex{Tw}},
+    r1::T, r2::T, w::T, s::T, c::T;
+) where {Tw <: Breal, T <: Breal}
 
-    λ1, λ2, λ3 = eigvals_bloch3(r1, r2, w, s, c)
+    λ[1], λ[2], λ[3] = eigvals_bloch3(r1, r2, w, s, c)
 
-    V = hcat(
-        eigvec_bloch3(r1, r2, w, s, c, λ1),
-        eigvec_bloch3(r1, r2, w, s, c, λ2),
-        eigvec_bloch3(r1, r2, w, s, c, λ3)
-    )
+    eigvec_bloch3!((@view V[:,1]), row1, row2, row3, r1, r2, w, s, c, λ[1])
+    eigvec_bloch3!((@view V[:,2]), row1, row2, row3, r1, r2, w, s, c, λ[2])
+    eigvec_bloch3!((@view V[:,3]), row1, row2, row3, r1, r2, w, s, c, λ[3])
 
-    return [λ1, λ2, λ3], V
+    return λ, V
 end
 
 
+#=
 # helper for mismatched types
 function eigen_bloch3(
     r1::T1, r2::T2, w::Tw, s::Ts, c::Tc,
 ) where {T1 <: Breal, T2 <: Breal, Tw <: Breal, Ts <: Breal, Tc <: Breal}
     r1, r2, w, s, c, _ = promote(r1, r2, w, s, c, zero(Float32))
     return eigen_bloch3(r1, r2, w, s, c)
+end
+=#
+
+
+"""
+    expm_bloch3!(expAt, work, r1, r2, w, s, c, t)
+
+Return exp(A*t) for the 3×3 Bloch matrix
+`A = [-r2 w s; -w -r2 c; -s -c -r1].`
+Uses explicit eigendecomposition
+based on analytical roots of cubic characteristic polynomial.
+Mutates `expAt` and `work::Bloch3ExpmWorkspace`.
+Allocates very little memory; just some `lu!` overhead.
+"""
+function expm_bloch3!(
+    expAt::Matrix{Te},
+    work::Bloch3ExpmWorkspace{Tw},
+    r1::T, r2::T, w::T, s::T, c::T, t::Tt;
+) where {Te <: Breal, Tw <: Breal, T <: Breal, Tt <: Breal}
+
+    eigen_bloch3!(work.λ, work.V, work.row1, work.row2, work.row3,
+        r1, r2, w, s, c) # eigendecomposition of A
+
+#=
+    # If V is ill-conditioned (near defective/repeated eigenvalues), fall back
+    # No! Cannot compute `cond` for dual types
+    if cond(V) > 1e12 || any(isnan, V) || any(isinf, V)
+
+        A = [-r2  w  s;
+             -w -r2  c;
+             -s -c  -r1]
+        return exp(t*A)  # Julia's matrix exponential
+    end
+=#
+
+    # compute V * D(exp(λ t)) * V^{-1}
+    # using lu! and rdiv! to minimize allocations
+    tmp1 = work.row1
+    @. tmp1 = exp(t * work.λ)
+    Dexp = Diagonal(tmp1)
+    mul!(work.VD, work.V, Dexp)
+    rdiv!(work.VD, lu!(work.V)) # V * D * V^-1
+
+    # inputs are real, so result should be real up to numerical roundoff
+    @. expAt = real(work.VD)
+    return expAt
 end
 
 
@@ -169,26 +316,11 @@ Uses explicit eigendecomposition
 based on analytical roots of cubic characteristic polynomial.
 """
 function expm_bloch3(
-    r1::T1, r2::T2, w::Tw, s::Ts, c::Tc, t::Tt,
+    r1::T1, r2::T2, w::Tw, s::Ts, c::Tc, t::Tt;
 ) where {T1 <: Breal, T2 <: Breal, Tw <: Breal, Ts <: Breal, Tc <: Breal, Tt <: Breal}
-
-    (λ, V) = eigen_bloch3(r1, r2, w, s, c)
-
-#=
-    # If V is ill-conditioned (near defective/repeated eigenvalues), fall back
-    # Cannot compute `cond` for dual types
-    if cond(V) > 1e12 || any(isnan, V) || any(isinf, V)
-
-        T = promote_type(T1, T2, Tw, Ts, Tc, Tt)
-        A = [-r2  w  s;
-             -w -r2  c;
-             -s -c  -r1]
-        return exp(t*A)  # Julia's matrix exponential
-    end
-=#
-
-    Dexp = Diagonal(exp.(t * λ))
-    tmp = V * Dexp * inv(V)
-    # Inputs are real; result should be real up to numerical roundoff
-    return real(tmp)
+    T = promote_type(T1, T2, Tw, Ts, Tc, Tt)
+    expAt = Matrix{T}(undef, 3, 3)
+    work = Bloch3ExpmWorkspace{T}()
+    expm_bloch3!(expAt, work, r1, r2, w, s, c, t)
+    return expAt
 end
--- test/expm-bloch3.jl
+++ test/expm-bloch3.jl
@@ -1,10 +1,11 @@
 # expm-bloch3.jl
 
-using BlochSim: expm_bloch3
-using BlochSim: matrix_bloch3, eigvals_bloch3, eigvec_bloch3, eigen_bloch3
-import ForwardDiff
+using BlochSim: expm_bloch3, expm_bloch3!
+using BlochSim: matrix_bloch3, eigvals_bloch3
+using BlochSim: Bloch3ExpmWorkspace, cross!, eigvec_bloch3!, eigen_bloch3!
 using ExponentialAction: expv
-using LinearAlgebra: eigvals, eigvecs, eigen, Diagonal, I
+import ForwardDiff
+using LinearAlgebra: cross, eigvals, eigvecs, eigen, Diagonal, I
 using Test: @inferred, @test, @testset, @test_throws
 
 
@@ -27,46 +28,84 @@ compare_eigs(eig_la::Vector{<:Complex}, eig_b3) =
     s, c = sincos(ϕ) # for speed and consistency
     s *= Ω
     c *= Ω
-    A = @inferred matrix_bloch3(r1, r2, w, s, c)
+    xp = [r1, r2, w, s, c] # random parameters for testing
+
+    # helper
+    A = @inferred matrix_bloch3(xp...)
+
 
     # check eigenvalues
-    @inferred eigvals_bloch3(Float32.((2, 1, 1, 0, 0))...) # Δ > 0 branch
-    @inferred eigvals_bloch3(Float32.((3, 1, 0, 0, 0.8))...) # Δ < 0 branch
-    @inferred eigvals_bloch3(r1, r2, w, s, Float16(c)) # helper
-    lam3 = @inferred eigvals_bloch3(r1, r2, w, s, c)
+    x1 = (2, 1, 1, 0, 0) # Δ > 0
+    x2 = (3, 1, 0, 0, 0.8) # Δ < 0
+    @test 0 == @allocated eigvals_bloch3(x1...) # Δ > 0
+    @test 0 == @allocated eigvals_bloch3(x2...) # Δ < 0
+    @inferred eigvals_bloch3(Float32.(x1)...) # Δ > 0 branch
+    @inferred eigvals_bloch3(Float32.(x2)...) # Δ < 0 branch
+    @inferred eigvals_bloch3(2, 1, 0, 0, 0f0) # helper
+    lam3 = @inferred eigvals_bloch3(xp...)
     eig = eigen(A)
     @test compare_eigs(eig.values, lam3)
 
-    @inferred eigvec_bloch3(r1, r2, w, s, c, ComplexF16(0)) # helper
+
+    # check cross!
+    v = Vector{ComplexF64}(undef, 3)
+    a = [0, 1//2, 4f0]
+    b = [1.0, 2, complex(3,4)]
+    @inferred cross!(v, a, b) # warm-up too
+    @test 0 == @allocated cross!(v, a, b)
+    @test v ≈ cross(a, b)
+
+
+    # check eigvecs
+    row1 = Vector{ComplexF64}(undef, 3)
+    row2 = Vector{ComplexF64}(undef, 3)
+    row3 = Vector{ComplexF64}(undef, 3)
+    v = Vector{ComplexF64}(undef, 3)
+    @inferred eigvec_bloch3!(v, row1, row2, row3, 2, 1//1, 0, 0, 0, Complex(0))
+    eigvec_bloch3!(v, row1, row2, row3, x1..., Complex(0)) # warm-up
+    @test 0 == @allocated eigvec_bloch3!(v, row1, row2, row3, 2, 1, 1, 0, 0, Complex(0))
+
 
     # check eigendecomposition
-    @inferred eigen_bloch3(r1, r2, w, s, Float16(c)) # helper
-    λ3, V3 = @inferred eigen_bloch3(r1, r2, w, s, c)
-    @test compare_eigs(eig.values, λ3)
+    T = Float64
+    C = Complex{T}
+    work = Bloch3ExpmWorkspace{T}()
+    arg = (work.λ, work.V, work.row1, work.row2, work.row3)
+    xt = (2., 1., 0., 0., 0.)
+    @inferred eigen_bloch3!(arg..., xt...) # warm up
+    @test 0 == @allocated eigen_bloch3!(arg..., xt...)
 
+    λ3 = Vector{C}(undef, 3)
+    V3 = Matrix{C}(undef, 3, 3)
+    @inferred eigen_bloch3!(λ3, V3, work.row1, work.row2, work.row3, xp...)
+    @test compare_eigs(eig.values, λ3)
     @test A ≈ V3 * Diagonal(λ3) * inv(V3)
 
 
     # check exp
+    expAt = Matrix{T}(undef, 3, 3)
+    @inferred expm_bloch3!(expAt, work, xp..., t) # warm up
+    @test 100 ≥ @allocated expm_bloch3!(expAt, work, xp..., t)
+
     E0 = exp(A * t)
-    E1 = expm_bloch3(r1, r2, w, s, c, t)
+    @test expAt ≈ E0
+
+    E1 = expm_bloch3(xp..., t)
     @test E1 ≈ E0
 
     Ev = expv(t, A, I(3))
     @test Ev ≈ E0
 
-
     # autodiff
-    x = [r1, r2, w, s, c]
     f3(x) = expm_bloch3(x..., t)
-    @test f3(x) == E1
-    jac3 = ForwardDiff.jacobian(f3, x)
+    @test f3(xp) == E1
+    jac3 = ForwardDiff.jacobian(f3, xp)
 
     fv(x) = expv(t, matrix_bloch3(x...), I(3))
-    jacv = ForwardDiff.jacobian(fv, x)
+    jacv = ForwardDiff.jacobian(fv, xp)
     @test jac3 ≈ jacv
 
-    ft(t) = expm_bloch3(x..., only(t))
+    ft(t) = expm_bloch3(xp..., only(t))
     jact = ForwardDiff.jacobian(ft, [t])
     tmp = reshape(jact, 3, 3)
     @test tmp ≈ A * ft([t]) # check d/dt exp(A*t) = A * exp(A*t)
