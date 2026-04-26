1a206f0a23a07043cae299de5d77211481c9c636
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-04-26 17:57:30 -0400

    plot comparisons

--- docs/lit/examples/01-rotate.jl
+++ docs/lit/examples/01-rotate.jl
@@ -1,9 +1,10 @@
 #=
-# [Rotation](@id rotation)
+# [Rotation conventions](@id rotation)
 
-This page discusses the Julia package
+This page discusses
+the rotation conventions of
+the Julia package
 [`BlochSim`](https://github.com/StevenWhitaker/BlochSim.jl)
-especially its rotation conventions.
 =#
 
 #srcURL
@@ -40,7 +41,7 @@ using MIRTjim: prompt
 using Plots: annotate!, color, default, gui, plot, plot!, scatter!, scatter3d!, text
 
 default(titlefontsize = 10, markerstrokecolor = :auto, label="", width = 1.5,
-    linewidth=2)
+    linewidth = 2)
 
 
 # The following line is helpful when running this file as a script;
@@ -52,7 +53,8 @@ isinteractive() || prompt(:draw);
 #=
 ## Rotation convention
 
-The RF excitation convention used in `BlochSim`
+The convention for rotation by RF excitation
+in `BlochSim`
 is illustrated by the following plots.
 =#
 
@@ -60,26 +62,26 @@ round2(x) = round(x, digits = 2)
 
 Mz0, T1_ms, T2_ms, Δf_Hz = 1, 400, 10, 9 # tissue parameters
 xt = (; Mz0, T1_ms, T2_ms, Δf_Hz) # tuple
-α_deg = 90 # flip angle °
-α_rad = deg2rad(α_deg)
 spin = Spin(Mz0, T1_ms, T2_ms, Δf_Hz)
 
 
-# helper
-function plot_tip(θ; α=π/2, color=:blue, excite=excite)
-    v = nothing
-    tmp = [1, 1, 1]
+# Helper function to show rotation from equilibrium to transverse plane
+function plot_tip(θ; # phase angle
+    α=π/2, # flip angle (radians)
+    color=:blue, excite=excite,
+)
     p = plot(
-     xaxis = ("", (-1, 1), tmp, ),
-     yaxis = ("", (-1, 1), tmp, ),
-     zaxis = ("", (-1, 1), tmp, ),
+     xaxis = ("", (-1, 1), ), xtick = [1],
+     yaxis = ("", (-1, 1), ), ytick = [1],
+     zaxis = ("", (-1, 1), ), ztick = [1],
      aspect_ratio = 1,
      framestyle = :origin,
      size = (400, 400),
      camera = (139, 30),
      title = "θ = $(round2(rad2deg(θ)))°",
     )
-    plot!([0, -sin(θ)], [0, -cos(θ)], [0, 0], lw=3, color=:black) # B1
+
+    plot!([0, -sin(θ)], [0, -cos(θ)], [0, 0], lw=3, color=:black) # B₁
 
     for α in range(0, α, 11)
         rf = InstantaneousRF(α, θ)
@@ -87,20 +89,21 @@ function plot_tip(θ; α=π/2, color=:blue, excite=excite)
         v = Matrix(A) * [0, 0, 1]
         plot!([0, v[1]], [0, v[2]], [0, v[3]], lw=2; color)
         scatter3d!([v[1]], [v[2]], [v[3]],
-            markercolor = color, 
+            markercolor = color,
             markershape = :utriangle, # lazy arrow tip
         )
     end
 
-    annotate!([
-        (1.3, 0, 0, text("x", :left, 10, :bold)),
-        (0, 1.2, 0, text("y", :left, 10, :bold)),
-        (0, 0, 1.2, text("z", :bottom, 10, :bold)),
+    annotate!(p, [
+        (1.3, 0, 0, text("x", :left, 10)),
+        (0, 1.2, 0, text("y", :left, 10)),
+        (0, 0, 1.2, text("z", :bottom, 10)),
     ])
     return p
 end;
 
-function plot_tip3( ; excite=excite)
+# Helper to show 3 different θ cases:
+function plot_tips( ; excite=excite)
     return plot(
      plot_tip(  0, color=:blue; excite),
      plot_tip(π/4, color=:magenta; excite),
@@ -109,7 +112,7 @@ function plot_tip3( ; excite=excite)
      size = (900, 300),
     )
 end
-ptip1 = plot_tip3( ; excite=excite)
+ptip1 = plot_tips( ; excite=excite)
 
 #
 prompt()
@@ -124,12 +127,12 @@ about an axis in the x-y plane
 that makes left-handed angle `θ`
 with the negative y-axis."
 
-Comments within the function
+[Comments within the function](https://github.com/MagneticResonanceImaging/BlochSim.jl/blob/2b13b1858efba2897ebdbdc8e30cb73807b20cf0/src/excite.jl#L165)
 refer to p.27 of Dwight Nishimura's
 ["Principles of Magnetic Resonance Imaging" (1996).](https://books.google.com/books/about/Principles_of_Magnetic_Resonance_Imaging.html?id=uz9BAQAAIAAJ)
 
-As illustrated by black line above,
-the B1 axis is
+As illustrated by the black line above,
+the B₁ axis is
 `[-sin(θ) -cos(θ) 0]`
 
 So `α = π/2` and `θ = 0`
@@ -141,6 +144,10 @@ to
 The following excitation matrix examples illustrate.
 =#
 
+# ### θ=0
+
+α_deg = 90 # flip angle °
+α_rad = deg2rad(α_deg)
 rf0 = InstantaneousRF(α_rad, 0)
 A1, _ = excite(spin, rf0)
 round2.(Matrix(A1))
@@ -149,7 +156,7 @@ round2.(Matrix(A1))
 round2.(Matrix(A1) * [0, 0, 1])
 
 
-# ## θ=π/2
+# ### θ=π/2
 
 #
 rf2 = InstantaneousRF(α_rad, π/2)
@@ -159,8 +166,17 @@ round2.(Matrix(A2))
 #
 round2.(Matrix(A2) * [0, 0, 1])
 
-# todo: more discussion
-ptip3 = plot_tip3( ; excite=excite_bloch3)
+#=
+The following figure uses the function `excite_bloch3`
+that is based on analytical solutions
+to the eigenvalues of the 3×3 Bloch matrix.
+It is designed to use the same rotation conventions
+for self consistency.
+
+=#
+ptip3 = plot_tips( ; excite = (spin, rf) -> excite_bloch3(spin, rf; warn=false))
+
+#
 prompt()
 
 #src include("../../../inc/reproduce.jl")
