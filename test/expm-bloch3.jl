e6de7276f803e9201c8859b637782a7a4f9cafea
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-04-26 21:37:27 -0400

    test excite with b1_gauss RF1

--- test/expm-bloch3.jl
+++ test/expm-bloch3.jl
@@ -1,7 +1,7 @@
 # expm-bloch3.jl
 
-using BlochSim: Spin, excite, InstantaneousRF
-using BlochSim: expm_bloch3, expm_bloch3!
+using BlochSim: Spin, excite, GAMMA, InstantaneousRF, RF
+using BlochSim: expm_bloch3, expm_bloch3!, b1_gauss, RF1
 using BlochSim: excite_bloch3, excite_bloch3!, _TE_ms
 using BlochSim: matrix_bloch3, matrix_bloch3!, eigvals_bloch3, eigvec_3by3!
 using BlochSim: Bloch3ExpmWorkspace, cross!, eigvec_bloch3!, eigen_bloch3!
@@ -143,13 +143,34 @@ compare_eigs(eig_la::Vector{<:Complex}, eig_b3) =
     @test (expAt, b1) == excite_bloch3(xp..., t)
     @test 0 == @inferred _TE_ms(Val(:postRF), Inf)
 
-    # w0 = 2π * (Δf_Hz/1000) # rad/ms
-    Δf_Hz = w * 1000 / 2π
+    Δf_Hz = w * 1000 / 2π # w0 = 2π * (Δf_Hz/1000) # rad/ms
+    α_rad = π/3
+    θ = π/5
+    begin
+        spin0 = Spin(1, 1e9, 1e8, 1e-8*Δf_Hz)
+        rf0 = InstantaneousRF(α_rad, θ)
+        expA3, b3 = @inferred excite_bloch3(spin0, rf0; warn = false)
+        expAe, be = excite(spin0, rf0)
+        expAe = Matrix(expAe)
+        @test maximum(abs, b3) < 1e-11
+        @test maximum(abs, expAe - expA3) < 2e-8
+    end
+
     spin = Spin(1, 1000/r1, 1000/r2, Δf_Hz)
-    rf = InstantaneousRF(π/4, π/3)
-    expA3, b3 = @inferred excite_bloch3(spin, rf; warn = false)
-    expAe, be = excite(spin, rf)
-    @test maximum(abs, b3) < 1e-11 # because of r1_tiny
+    tRF_ms = 2
+    rf1 = RF1(α_rad, tRF_ms, θ)
+
+    # test single-sample bloch3 vs many-sample BlochSim
+    nw = 2000
+    waveform = fill(cis(θ), nw) * b1_gauss(α_rad, tRF_ms)
+    rf2 = RF(waveform, tRF_ms / nw) # multi-sample RF waveform
+
+    A3, b3 = @inferred excite_bloch3(spin, rf1)
+    A1, b1 = @inferred excite(spin, rf2)
+    A1 = Matrix(A1)
+    b1 = Vector(b1)
+    @test maximum(abs, b1 - b3) < 1e-9
+    @test maximum(abs, A1 - A3) < 1e-7
 
 
     # autodiff
