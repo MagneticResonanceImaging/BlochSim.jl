c197431b14fc7028b76cea384b996e246f276f5a
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-05-02 17:02:36 +0200

    ignore ADTypes by aqua

--- test/aqua.jl
+++ test/aqua.jl
@@ -5,5 +5,9 @@ using Test: @testset
 @testset "aqua" begin
     Aqua.test_ambiguities(BlochSim; broken = true)
     Aqua.test_unbound_args(BlochSim; broken = true)
-    Aqua.test_all(BlochSim; ambiguities = false, unbound_args = false) # todo
+    Aqua.test_all(BlochSim;
+       ambiguities = false,
+       unbound_args = false, # todo
+       stale_deps = (ignore = [:ADTypes],), # used by Optim extension
+    )
 end
